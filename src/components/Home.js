4e9e9c7bf4e6bc7b8686ddc68d0d5282e1e3508c
DucThien <thienbanho@gmail.com>
2024-10-31 19:41:15 +0700

    1st commit UI

---
+++ src/components/Home.js
@@ -0,0 +1,218 @@
+import Logo from "../assert/logo.png";
+import LogoSearch from "../assert/search.png";
+import LogoProfile from "../assert/profile-user.png";
+import LogoCart from "../assert/shopping-cart.png";
+
+function Home() {
+  return (
+    <div className="Home">
+        <div className="nav_bar">
+          <img className="logo" src={Logo} alt="Logo"></img>
+          <ul class="nav-links">
+            <li>
+              <a href="#">HOME</a>
+            </li>
+            <li>
+              <a href="#">PRODUCTS</a>
+            </li>
+            <li>
+              <a href="#">CONTACT</a>
+            </li>
+            <li>
+              <a href="#">ABOUT</a>
+            </li>
+          </ul>
+          <div className="icon">
+            <a href="#"><img src={LogoSearch} alt="search"></img></a>
+            <a href="#"><img src={LogoProfile} alt="profile"></img></a>
+            <a href="#"><img src={LogoCart} alt="cart"></img></a>
+          </div>
+        </div>
+
+        <p>awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        gkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        gkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        gkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        gkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        awgkawmgkawmgkamwgkawkgmwkagkmakwgm
+        </p>
+
+
+        <footer>
+      <section class="footer">
+        <div class="footer-row">
+          <div class="footer-col">
+            
+          </div>
+
+          <div class="footer-col">
+            <h3>Store Information</h3>
+            <ul class="links">
+              <li><a href="#">227 NVC, Quan 5, HCM</a></li>
+              <li><a href="#">0582079324</a></li>
+              <li><a href="#">fitnessstore@gmail.com</a></li>
+
+            </ul>
+          </div>
+
+          <div class="footer-col">
+          <h4>About us</h4>
+            <ul class="links">
+              <li><a href="#">Search</a></li>
+              <li><a href="#">Introduction</a></li>
+              <li><a href="#">Payment Change Policy</a></li>
+              <li><a href="#">Main Delivery</a></li>
+              <li><a href="#">Policy Payment</a></li>
+              <li><a href="#">Buying Guide</a></li>
+              <li><a href="#">Service Account</a></li>
+
+            </ul>
+          </div>
+
+          <div class="footer-col">
+            <h4>Sign up to receive promotions</h4>
+            <form action="#">
+              <input type="text" placeholder="Your email" required />
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </footer>
+    </div>
+
+  );
+}
+
+export default Home;
