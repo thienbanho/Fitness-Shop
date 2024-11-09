eeb3bc3c48e9fb63c37fcb4170457a5fd539c5e2
DucThien <thienbanho@gmail.com>
2024-11-09 09:15:27 +0700

    change folder structure

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/Footer/Footer"
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Footer></Footer>
+
+    </>
+  );
+}
+
+export default Home;
