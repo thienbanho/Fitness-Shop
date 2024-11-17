d11a5ae4584fcd9a8851931ff1a8268873c96899
DucThien <thienbanho@gmail.com>
2024-11-17 08:36:42 +0700

    fix import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./pages/Home/Home.js";
 import Login from "./pages/LoginSignUp/Login.js"
+import Coupon from "./pages/Profile/Coupon Wallet/coupon_wallet.js";
+import Info from "./pages/Profile/Info/info.js";
 
 function App() {
   return (
-    <Login/>
+    <Info/>
   );
 }
 
--- src/pages/Profile/Info/info.js
+++ src/pages/Profile/Info/info.js
@@ -1,5 +1,5 @@
-import NavBar from "../../components/NavBar/NavBar";
-import Footer from "../../components/Footer/Footer";
+import NavBar from "../../../components/NavBar/NavBar";
+import Footer from "../../../components/Footer/Footer";
 import "./info.css";
 import React from "react";
 
