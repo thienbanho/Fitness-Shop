c938d73418af6cfd73859128a992056962ec055a
Viết Long <125510300+viertIlong@users.noreply.github.com>
2024-11-22 01:57:50 +0700

    add order management

---
+++ src/pages/Profile/Order/order.js
@@ -0,0 +1,30 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import "./order.css";
+import userDashboard from "../../components/User Dashboard/userDashboard";
+
+function orderManagement() {
+  return (
+    <>
+      <NavBar />
+      <div className="order-management">
+        <userDashboard />
+        
+        <div className="order-container">
+            <div className="order-header">
+                <h1>Order management</h1>
+            </div>
+
+            <div className="order-sub-header">
+                <a>Shipping</a>
+                <a>Delivered</a>
+            </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default OrderManagement;
