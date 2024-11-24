97facf38a6adb7ed9cf96b51b4d7d5ef2b43cb03
Viết Long <125510300+viertIlong@users.noreply.github.com>
2024-11-24 22:06:24 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home.js";
 import Login from "./pages/LoginSignUp/Login.js"
 import PTRegist from "./pages/PTRegist/PTRegist.js";
 import Product from "./pages/Product/Product.js";
+import Info from "./pages/Profile/Info/info.js";
 import NavBar from "./components/NavBar/NavBar.js";
 import Footer from "./components/Footer/Footer.js";
 import { BrowserRouter } from 'react-router-dom';
