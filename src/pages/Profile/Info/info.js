6392fa760f117751f72b98e983e3c4fae1fa3b6a
Viết Long <125510300+viertIlong@users.noreply.github.com>
2024-11-24 22:05:13 +0700

    fix

--- src/pages/Profile/Info/info.js
+++ src/pages/Profile/Info/info.js
@@ -1,7 +1,7 @@
 import React from "react";
-import NavBar from "../../components/NavBar/NavBar";
-import Footer from "../../components/Footer/Footer";
-import UserDashboard from "../../components/User Dashboard/userDashboard";
+import NavBar from "../../../components/NavBar/NavBar";
+import Footer from "../../../components/Footer/Footer";
+import UserDashboard from "../../../components/User Dashboard/userDashboard";
 import "./info.css";
 
 function Info() {
